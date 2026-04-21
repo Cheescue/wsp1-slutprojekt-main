b8dc9bd2677eac66ad5a644f446393e0f28b63ec
Cheescue <alexandar.drobnjakk@gmail.com>
2026-04-21 15:53:30 +0200

    2026-04-21

--- Gemfile
+++ Gemfile
@@ -7,6 +7,9 @@ gem 'sqlite3'
 gem 'webrick'
 gem 'awesome_print'
 gem 'debug'
+gem 'bcrypt'
+gem 'securerandom'
+
 
 group :development do
   gem 'sinatra-contrib' # Replaces 'rerun' for better Ruby 4.0 compatibility
--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require 'debug'
 require 'awesome_print'
+require 'bcrypt'
+require 'securerandom'
 
 class App < Sinatra::Base
 
@@ -12,6 +14,18 @@ class App < Sinatra::Base
     @db.results_as_hash = true
     @db
   end
+  configure do
+    enable :sessions
+    set :session_secret, SecureRandom.hex(64)
+  end
+
+  before do
+    ap session[:user_id]
+    if session[:user_id]
+      @current_user = db.execute("SELECT * FROM users WHERE id = ?", session[:user_id]).first
+      ap @current_user
+    end
+  end
 
   get '/' do
     redirect '/pizzashoppen'
@@ -19,7 +33,6 @@ class App < Sinatra::Base
 
   get '/pizzashoppen' do
     @pizza = db.execute("SELECT * FROM pizza")
-    ap @pizza
     erb :"pizzashoppen/index"
   end
 
@@ -54,6 +67,71 @@ class App < Sinatra::Base
     db.execute('UPDATE pizza SET name = ?, price = ? WHERE id = ?', [name, price, id])
 
     redirect '/pizzashoppen'
+  end
+
+  get '/login' do
+    erb(:"pizzashoppen/login")
+  end
+
+  post '/login' do
+    request_username = params[:username]
+    request_plain_password = params[:password]
+
+    user = db.execute("SELECT *
+            FROM users
+            WHERE username = ?",
+            request_username).first
+
+    unless user
+      ap "/login : Invalid username."
+      status 401
+      redirect "/login"
+    end
+
+    db_id = user["id"].to_i
+    ap db_id
+    db_password_hashed = user["password"].to_s
+
+    bcrypt_db_password = BCrypt::Password.new(db_password_hashed)
+    if bcrypt_db_password == request_plain_password
+      ap "/login : Logged in "
+      session[:user_id] = db_id
+      ap session[:user_id]
+      redirect("/")
+    else
+      ap "/login : Invalid password."
+      status 401
+      redirect '/login'
+    end
+  end
+
+  post '/logout' do
+    ap "Logging out"
+    session.clear
+    redirect '/'
+  end
+
+  get '/signup' do
+    erb(:"pizzashoppen/signup")
+  end
+
+  post '/signup' do
+    request_username = params[:username]
+    request_plain_password = params[:password]
+
+    password_hashed = BCrypt::Password.create(request_plain_password)
+
+    db.execute("INSERT INTO users (username, password) VALUES (?,?)", [request_username, password_hashed])
+
+    user = db.execute("SELECT * FROM users WHERE username = ? AND password = ?", [request_username, password_hashed]).first
+    db_id = user["id"].to_i
+    session[:user_id] = db_id
+    ap session[:user_id]
+    ap "FUCK FUCK FUCK"
+
+    redirect('/')
+
+
   end
 
 end
